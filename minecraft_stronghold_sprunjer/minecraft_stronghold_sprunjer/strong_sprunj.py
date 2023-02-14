3a849b54d1f170884fd0119c221092886429084c
Jennifer Pearl <jenpearl5@gmail.com>
2023-02-14 08:29:11 +0000

    backup

---
+++ minecraft_stronghold_sprunjer/minecraft_stronghold_sprunjer/strong_sprunj.py
@@ -0,0 +1,19 @@
+
+
+
+class MinecraftCoordinate(object):
+    def __init__(self, x: float = 0, y:float = 0, z: float = 0, heading:float = 0) -> None:
+        self._X: float = x
+        self._Y: float = y
+        self._Z: float = z
+        self._heading: float = heading
+
+
+
+
+
+class StrongholdSprunjer:
+    """
+    Class for the stronghold sprunjing to happen at
+    """
+    south: float = 0
